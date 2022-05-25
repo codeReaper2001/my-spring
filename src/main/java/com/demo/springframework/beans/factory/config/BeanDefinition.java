1f02d2abeff01b4bc198d0393e0b138c2d98035e
codeReaper2001 <2210086948@qq.com>
2022-05-25 17:37:03 +0800

    04-为Bean对象注入属性和依赖Bean的功能实现

--- src/main/java/com/demo/springframework/beans/factory/config/BeanDefinition.java
+++ src/main/java/com/demo/springframework/beans/factory/config/BeanDefinition.java
@@ -1,17 +1,34 @@
 package com.demo.springframework.beans.factory.config;
 
-public class BeanDefinition {
-    private Class beanClass;
+import com.demo.springframework.beans.PropertyValues;
 
-    public BeanDefinition(Class beanClass) {
-        this.beanClass = beanClass;
+public class BeanDefinition {
+    private Class<?> beanClass;
+
+    private PropertyValues propertyValues;
+
+    public BeanDefinition(Class<?> beanClass) {
+        this(beanClass, null);
     }
 
-    public Class getBeanClass() {
+    public BeanDefinition(Class<?> beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues != null ? propertyValues : new PropertyValues();
+    }
+
+    public Class<?> getBeanClass() {
         return beanClass;
     }
 
-    public void setBeanClass(Class beanClass) {
+    public void setBeanClass(Class<?> beanClass) {
         this.beanClass = beanClass;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
     }
 }
--- src/test/java/com/demo/springframework/test/ApiTest.java
+++ src/test/java/com/demo/springframework/test/ApiTest.java
@@ -1,5 +1,9 @@
 package com.demo.springframework.test;
 
+import com.demo.springframework.beans.PropertyValue;
+import com.demo.springframework.beans.PropertyValues;
+import com.demo.springframework.beans.factory.config.BeanReference;
+import com.demo.springframework.test.bean.UserDao;
 import com.demo.springframework.test.bean.UserService;
 import com.demo.springframework.beans.factory.config.BeanDefinition;
 import com.demo.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -10,13 +14,20 @@ public class ApiTest {
     public void test_BeanFactory() {
         // 1.初始化 BeanFactory
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        // 2.注册bean
-        BeanDefinition beanDefinition = new BeanDefinition(UserService.class);
+        // 2.UserDao注册
+        beanFactory.registerBeanDefinition("userDao", new BeanDefinition(UserDao.class));
+
+        // 3.UserService设置属性[uId、userDao]
+        PropertyValues propertyValues = new PropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("uId", "10001"));
+        propertyValues.addPropertyValue(new PropertyValue("userDao", new BeanReference("userDao")));
+
+        // 4.UserService注入bean
+        BeanDefinition beanDefinition = new BeanDefinition(UserService.class, propertyValues);
         beanFactory.registerBeanDefinition("userService", beanDefinition);
 
-        //3.获取bean
-        UserService userService = (UserService)beanFactory.getBean("userService", "小傅哥");
-        System.out.println(userService);
+        // 5.UserService获取bean
+        UserService userService = (UserService) beanFactory.getBean("userService");
         userService.queryUserInfo();
     }
 }
